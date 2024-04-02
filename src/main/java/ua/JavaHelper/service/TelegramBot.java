bcb570c95d57a8a95dccf49f96e02d0a150322d5
wubulab <dmitrij.kaniuk@gmail.com>
2024-04-02 23:05:50 +0300

    Add basic funcs

--- src/main/java/ua/JavaHelper/service/TelegramBot.java
+++ src/main/java/ua/JavaHelper/service/TelegramBot.java
@@ -2,24 +2,63 @@ package ua.JavaHelper.service;
 
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ua.JavaHelper.config.BotConfig;
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
+    final BotConfig config;
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+    }
+
+
     @Override
     public String getBotUsername() {
-        return null;
+        return config.getBotName();
     }
     @Override
     public String getBotToken() {
-        return null;
+        return config.getToken();
     }
 
     @Override
     public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
 
+            switch (messageText) {
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                default:
+                    sendMessage(chatId, "Sorry, command not found");
+                    break;
+            }
+        }
+    }
+    private void startCommandReceived(long chatId, String userName) {
+        String answer = "Hi, " + userName + " " + "nice to meet you!";
+        sendMessage(chatId, answer);
     }
 
+    private void sendMessage(long chatId, String textToSend) {
 
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(String.valueOf(chatId)); //chatId);
+        sendMessage.setText(textToSend);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
+
+
